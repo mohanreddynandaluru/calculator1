dbdcb21f8e60ddb8d41b4aaefddc690b3dfeced7
Sravani Somavarapu <shivakumarsomavarapu331@gmail.com>
2026-03-06 16:03:39 +0530

    Added comment line for subtraction feature

---
+++ Subtraction.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace calculator
+{
+    public class Subtraction
+    {
+        public void doSubtraction(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine("Subtraction Result: " + result);
+        }
+    }
+}
+// added by shivakumar
