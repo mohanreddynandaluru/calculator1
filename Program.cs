ad2e2f292a9608633ba951f2b26b0613ec04080e
mohanreddynandaluru <mohanreddy232312@gmail.com>
2026-03-06 15:47:12 +0530

    calculator

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace calculator1{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition add = new Addition();
+            add.doAddition(5, 10);
+
+            Subtraction sub = new Subtraction();
+            sub.doSubtraction(20, 10);
+
+            Multiplication mult = new Multiplication();
+            mult.doMultiplication(20, 10);
+        }
+
+    }
+}
