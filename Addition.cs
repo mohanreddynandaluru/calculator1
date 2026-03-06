af9809b25b1c37ad84f7a284f667cb9b9cfe729b
Keshav2102 <kulkarnikeshav.kk@gmail.com>
2026-03-06 15:56:43 +0530

    Implement Addition class for basic addition

---
+++ Addition.cs
@@ -0,0 +1,13 @@
+using System;
+ 
+namespace calculator
+{
+    public class Addition
+    {
+        public void doAddition(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine("Addition Result: " + result);
+        }
+    }
+}
